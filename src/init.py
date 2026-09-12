25df78b3178fd4e7bd8bfa06e79d34d3c2375617
konstantin.aksenov <konstantin.aksenov@mstroy.tech>
2026-09-12 18:56:26 +0500

    feat: lesson 4

--- src/init.py
+++ src/init.py
@@ -1,59 +1,53 @@
+import pandas as pd
 import numpy as np
+import seaborn as sns
+import gdown as gd
 
-lst = [11, 22, 33, 44, 55, 66, 77, 88, 99]
-mass = np.array(lst, dtype=np.int32)
-print(mass)
-print(len(mass))
-print(sum(mass))
-print(mass[2])
-print(mass[[4,2,7,0,1]]) # выводит набор по индексам
-print(mass[mass > 50])
-print(2*mass+10) # применяется ко всем элементам массива
-gen_X = np.arange(12)
-print(gen_X)
-matrix_gen_X = gen_X.reshape(3, 4)
-print(matrix_gen_X)
-print(matrix_gen_X.tolist()[1][1], matrix_gen_X.tolist())
-gen_random = np.random.randint(150, 500, size=(5,5))
-print(gen_random)
-print(gen_random.min(axis=0)) #по столбцам
-print(gen_random.min(axis=1)) #по строкам
-print(gen_random.mean(axis=0)) #среднее
-a = np.arange(48).reshape(4,3,4)
-print(a)
-print(a.ndim) # размерность
-print(a.shape) # размер
-print(a.size)
-b = a + 1
+print(pd.__version__)
 
-print(b, b+a)
+num_list = [1,3,5,np.nan,6,8]
+numbers_series = pd.Series(num_list, name = 'numbers')
+print(numbers_series)
+frame = numbers_series.to_frame(name = 'numbers')
+print(frame)
 
-a = np.array([[1, 20, 0, 6],
-              [5, 4, 7, 8.0],
-              [9, 0, 110,0],
-              [2, 4, 5, 6]])
-print(np.sin(a))
-print(np.cos(a))
-print(np.tan(a))
-print(np.sin(45))
-print(np.cos(90))
-print(np.tan(45))
-a = np.array([[1, 20, 0, 6],
-              [5, 4, 7, 8.0],
-              [9, 0, 110,0],
-              [2, 4, 5, 6]])
-print(np.sqrt(a))
-print(np.sqrt(25))
+letter_list = ['a', 'b', 'c', 'd', 'e', np.nan]
+letter_series = pd.Series(letter_list, name='letters')
+print(letter_series)
 
-a = np.zeros((2, 4), int)
-print(a)
+print(pd.concat([numbers_series, letter_series], axis=1))
 
-a = np.ones((2, 3))
-print(a)
+df_other = pd.DataFrame({
+    'numbers': numbers_series,
+    'letters': letter_series
+})
+print(df_other)
 
+dates = pd.date_range('20260218', periods=450,freq='MS')
 
-a = np.array([[1, 2, 3, 4.0],
-              [5, 6, 7, 8],
-              [9, 10, 11, 12]])
-b = np.copy(a)
-print(b)
+numbers_for_df = np.random.randn(450, 4)
+
+df = pd.DataFrame(numbers_for_df, index=dates, columns=['A','B','C','D'])
+print(df)
+
+file = pd.read_csv("src/panda_data/5f6ce129fddc4ca0952f5e520e5f5bf8.csv", index_col=0)
+print(file)
+
+file_1 = sns.load_dataset('tips')
+print(file_1)
+
+file_1.to_csv("src/panda_data/test.csv", index=False)
+
+url = "https://drive.google.com/uc?id=1SA_V8uaKydrWJVaqUk3EJL1wjQD6CEvb"
+gd.download(url, 'src/panda_data/user_data.csv', quiet=True)
+
+# file_1.columns = [1,2,3,4,5,6,7] # переименовать колонки
+print(file_1)
+print(file_1.head(3))
+# print(file_1[2])
+
+print(file_1.T)
+file_1.sort_index(axis=0, ascending=False) # по умолчанию axis=0, т.е. сортировка по строкам
+file_1.sort_values(by=['total_bill'], ascending=False) # по умолчанию сортировка по индексу, выбрали столбцы
+print(file_1.sort_values(by=['total_bill'], ascending=False))
+# python ./src/init.py
